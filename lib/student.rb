84aae3b5f8d42f7c5242e9dc548c7e3ae4277503
Paris Dolfman <parisdolfman@gmail.com>
2020-06-30 17:22:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -67,7 +67,7 @@ end
 
   def self.find_by_name(name)
     sql = <<-SQL
-      SELECT * FROM students WHERE name = ?
+      SELECT * FROM students WHERE name = ? LIMIT 1
     SQL
 
     DB[:conn].execute(sql, name).map do |row|
