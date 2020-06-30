c0eddaa4761176984445ec8d2461647bce3fd5d2
Paris Dolfman <parisdolfman@gmail.com>
2020-06-30 17:02:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,10 +4,10 @@ class Student
 
   attr_accessor :name, :grade, :id
 
-  def initialize(name, grade, id=nil)
+  def initialize(id=nil,name, grade)
+    @id = id
     @name = name
     @grade = grade
-    @id = id
   end
 
   def self.create_table
@@ -56,6 +56,13 @@ end
     student = Student.new(name, grade)
     student.save
     student
+  end
+
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, grade)
   end
 
 end
